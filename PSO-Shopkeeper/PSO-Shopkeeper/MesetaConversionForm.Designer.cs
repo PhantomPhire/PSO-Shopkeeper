1d7c977ba1dbeb0e77fc9359605a375f7ae4ae03
Michael Oltmanns <mikeolt7@gmail.com>
2022-07-24 18:35:25 -0500

    Styled Meseta autofill form

--- PSO-Shopkeeper/PSO-Shopkeeper/MesetaConversionForm.Designer.cs
+++ PSO-Shopkeeper/PSO-Shopkeeper/MesetaConversionForm.Designer.cs
@@ -29,18 +29,20 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MesetaConversionForm));
-            this._goButton = new System.Windows.Forms.Button();
-            this._cancelButton = new System.Windows.Forms.Button();
+            this._goButton = new PSOShopkeeper.Controls.PSOShopkeeperButton();
+            this._cancelButton = new PSOShopkeeper.Controls.PSOShopkeeperButton();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this._pdValueTextBox = new System.Windows.Forms.TextBox();
             this._mesetaPerPDTextBox = new System.Windows.Forms.TextBox();
             this._abbrivateThousdandsCheck = new System.Windows.Forms.CheckBox();
+            this.psoShopkeeperPanel1 = new PSOShopkeeper.Controls.PSOShopkeeperPanel();
+            this.psoShopkeeperPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // _goButton
             // 
-            this._goButton.Location = new System.Drawing.Point(12, 97);
+            this._goButton.Location = new System.Drawing.Point(33, 152);
             this._goButton.Name = "_goButton";
             this._goButton.Size = new System.Drawing.Size(75, 23);
             this._goButton.TabIndex = 0;
@@ -50,7 +52,7 @@
             // 
             // _cancelButton
             // 
-            this._cancelButton.Location = new System.Drawing.Point(118, 97);
+            this._cancelButton.Location = new System.Drawing.Point(142, 152);
             this._cancelButton.Name = "_cancelButton";
             this._cancelButton.Size = new System.Drawing.Size(75, 23);
             this._cancelButton.TabIndex = 1;
@@ -61,31 +63,37 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(26, 55);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.Size = new System.Drawing.Size(85, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Max PD to Fill:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(26, 81);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.Size = new System.Drawing.Size(92, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Meseta Per PD:";
             // 
             // _pdValueTextBox
             // 
-            this._pdValueTextBox.Location = new System.Drawing.Point(93, 21);
+            this._pdValueTextBox.Location = new System.Drawing.Point(117, 55);
             this._pdValueTextBox.Name = "_pdValueTextBox";
             this._pdValueTextBox.Size = new System.Drawing.Size(100, 20);
             this._pdValueTextBox.TabIndex = 4;
             // 
             // _mesetaPerPDTextBox
             // 
-            this._mesetaPerPDTextBox.Location = new System.Drawing.Point(93, 47);
+            this._mesetaPerPDTextBox.Location = new System.Drawing.Point(117, 81);
             this._mesetaPerPDTextBox.Name = "_mesetaPerPDTextBox";
             this._mesetaPerPDTextBox.Size = new System.Drawing.Size(100, 20);
             this._mesetaPerPDTextBox.TabIndex = 5;
@@ -93,41 +101,60 @@
             // _abbrivateThousdandsCheck
             // 
             this._abbrivateThousdandsCheck.AutoSize = true;
-            this._abbrivateThousdandsCheck.Location = new System.Drawing.Point(15, 73);
+            this._abbrivateThousdandsCheck.BackColor = System.Drawing.Color.Transparent;
+            this._abbrivateThousdandsCheck.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._abbrivateThousdandsCheck.ForeColor = System.Drawing.SystemColors.Control;
+            this._abbrivateThousdandsCheck.Location = new System.Drawing.Point(33, 129);
             this._abbrivateThousdandsCheck.Name = "_abbrivateThousdandsCheck";
-            this._abbrivateThousdandsCheck.Size = new System.Drawing.Size(183, 17);
+            this._abbrivateThousdandsCheck.Size = new System.Drawing.Size(212, 17);
             this._abbrivateThousdandsCheck.TabIndex = 6;
             this._abbrivateThousdandsCheck.Text = "Abbreviate Thousands of Meseta";
-            this._abbrivateThousdandsCheck.UseVisualStyleBackColor = true;
+            this._abbrivateThousdandsCheck.UseVisualStyleBackColor = false;
+            // 
+            // psoShopkeeperPanel1
+            // 
+            this.psoShopkeeperPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.psoShopkeeperPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("psoShopkeeperPanel1.BackgroundImage")));
+            this.psoShopkeeperPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.psoShopkeeperPanel1.Controls.Add(this._pdValueTextBox);
+            this.psoShopkeeperPanel1.Controls.Add(this._mesetaPerPDTextBox);
+            this.psoShopkeeperPanel1.Controls.Add(this.label2);
+            this.psoShopkeeperPanel1.Controls.Add(this._abbrivateThousdandsCheck);
+            this.psoShopkeeperPanel1.Controls.Add(this.label1);
+            this.psoShopkeeperPanel1.Controls.Add(this._cancelButton);
+            this.psoShopkeeperPanel1.Controls.Add(this._goButton);
+            this.psoShopkeeperPanel1.Location = new System.Drawing.Point(-2, -25);
+            this.psoShopkeeperPanel1.Name = "psoShopkeeperPanel1";
+            this.psoShopkeeperPanel1.Size = new System.Drawing.Size(264, 208);
+            this.psoShopkeeperPanel1.TabIndex = 7;
+            this.psoShopkeeperPanel1.TitleText = "";
             // 
             // MesetaConversionForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(201, 130);
-            this.Controls.Add(this._abbrivateThousdandsCheck);
-            this.Controls.Add(this._mesetaPerPDTextBox);
-            this.Controls.Add(this._pdValueTextBox);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this._cancelButton);
-            this.Controls.Add(this._goButton);
+            this.ClientSize = new System.Drawing.Size(265, 184);
+            this.Controls.Add(this.psoShopkeeperPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MesetaConversionForm";
             this.Text = "Autofill Meseta";
+            this.psoShopkeeperPanel1.ResumeLayout(false);
+            this.psoShopkeeperPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button _goButton;
-        private System.Windows.Forms.Button _cancelButton;
+        private PSOShopkeeper.Controls.PSOShopkeeperButton _goButton;
+        private PSOShopkeeper.Controls.PSOShopkeeperButton _cancelButton;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox _pdValueTextBox;
         private System.Windows.Forms.TextBox _mesetaPerPDTextBox;
         private System.Windows.Forms.CheckBox _abbrivateThousdandsCheck;
+        private Controls.PSOShopkeeperPanel psoShopkeeperPanel1;
     }
 }
